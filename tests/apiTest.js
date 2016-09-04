5ac5e15c7b6182fc655aba6b40669bdae4529464
Dimitris Zervas <dzervas@dzervas.gr>
2016-09-04 23:36:52 +0300

    Finally fixed unit test

--- tests/apiTest.js
+++ tests/apiTest.js
@@ -1,18 +1,7 @@
 describe("showcasing a given deck", function() {
 	beforeEach(module("Yu-Gi-Oh"));
-	//beforeEach(function() {
-		//browser().navigateTo('/');
-	//});
-
-	//it("ensures we show the whole deck", inject(function(deck) {
-		//browser().navigateTo("#/");
-		//console.log(element("#sideDeck").children.length);
-		//expect(element("#sideDeck").children.length).toBe(deck.length);
-	//}));
 
 	it("ensures we can get card info", inject(["Card", function(Card) {
-		expect(Card.get).not.to.equal(null);
-
 		validCards = [
 			"Lyla, Lightsworn Sorceress",
 			"Monster Gate",
@@ -26,14 +15,18 @@ describe("showcasing a given deck", function() {
 		];
 
 		for (var i in validCards) {
-			var info = Card.get(validCards[i]);
-			expect(info.status == "success").toBeTruthy();
+			Card.get(validCards[i])
+				.success(function(info) {
+					expect(info.status == "success").toBeTruthy();
+				});
 		};
 
 		for (var i in invalidCards) {
-			var info = Card.get(invalidCards[i]);
-			expect(info.status == "fail").toBeTruthy();
-			expect(info.message == "No cards matching this name were found in our database.").toBeTruthy();
+			Card.get(invalidCards[i])
+				.error(function(info) {
+					expect(info.status == "fail").toBeTruthy();
+					expect(info.message == "No cards matching this name were found in our database.").toBeTruthy();
+				});
 		};
 
 	}]));
